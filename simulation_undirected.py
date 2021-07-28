ff35a54dbd6f92abafa953f02badcb5b1b508963
fraspass <fraspass@outlook.com>
2021-07-28 12:14:39 +0200

    try except

--- simulation_undirected.py
+++ simulation_undirected.py
@@ -46,30 +46,33 @@ for t in [None, 'normalised', 'theta']:
 ## Repeat M_sim times
 for s in range(M_sim):
     print('\rSimulated graph: ' + str(s+1), end='')
-    B = np.tril(np.random.uniform(size=(K,K)))
-    B += np.tril(B,k=-1).T
-    rho = np.random.beta(a=2,b=1,size=n)
-    ## Construct the adjacency matrix
-    A = np.zeros((n,n))
-    for i in range(n-1):
-        for j in range(i+1,n):
-            A[i,j] = np.random.binomial(n=1,p=rho[i]*rho[j]*B[z[i],z[j]],size=1)
-            A[j,i] = A[i,j]
-    Lambda, Gamma = np.linalg.eigh(A)
-    k = np.argsort(np.abs(Lambda))[::-1][:10]
-    X = np.dot(Gamma[:,k],np.diag(np.sqrt(np.abs(Lambda[k]))))
-    ## Remove empty rows
-    zero_index = np.array(A.sum(axis=0),dtype=int)
-    X = X[zero_index > 0]
-    zz = z[zero_index > 0]
-    ## Model setup
-    for d in range(1,6):
-        for k in range(2,7):
-            for t in [None, 'normalised', 'theta']:
-                M = dcsbm.EGMM(K=k)
-                z_est = M.fit_predict_approximate(X,d=d,transformation=t)
-                bics[t][s,d-1,k-2] += [M.BIC()]
-                aris[t][s,d-1,k-2] += [ari(z_est,zz)]
+    try:
+        B = np.tril(np.random.uniform(size=(K,K)))
+        B += np.tril(B,k=-1).T
+        rho = np.random.beta(a=2,b=1,size=n)
+        ## Construct the adjacency matrix
+        A = np.zeros((n,n))
+        for i in range(n-1):
+            for j in range(i+1,n):
+                A[i,j] = np.random.binomial(n=1,p=rho[i]*rho[j]*B[z[i],z[j]],size=1)
+                A[j,i] = A[i,j]
+        Lambda, Gamma = np.linalg.eigh(A)
+        k = np.argsort(np.abs(Lambda))[::-1][:10]
+        X = np.dot(Gamma[:,k],np.diag(np.sqrt(np.abs(Lambda[k]))))
+        ## Remove empty rows
+        zero_index = np.array(A.sum(axis=0),dtype=int)
+        X = X[zero_index > 0]
+        zz = z[zero_index > 0]
+        ## Model setup
+        for d in range(1,6):
+            for k in range(2,7):
+                for t in [None, 'normalised', 'theta']:
+                    M = dcsbm.EGMM(K=k)
+                    z_est = M.fit_predict_approximate(X,d=d,transformation=t)
+                    bics[t][s,d-1,k-2] += [M.BIC()]
+                    aris[t][s,d-1,k-2] += [ari(z_est,zz)]
+    except:
+        continue
 
 ## Obtain maximum
 def find_max(x):
