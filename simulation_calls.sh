3fae3b2ba445f4c976dfafbd4a3a1fd79654c25f
fraspass <fraspass@outlook.com>
2021-08-03 20:15:08 +0200

    asymptotic simulation calls

--- simulation_calls.sh
+++ simulation_calls.sh
@@ -1,56 +1,10 @@
-./simulation_undirected -n 100 -K 2 -s 1 & 
-./simulation_undirected -n 100 -K 2 -s 2 & 
-./simulation_undirected -n 100 -K 2 -s 3 & 
-./simulation_undirected -n 100 -K 2 -s 4 & 
-./simulation_undirected -n 100 -K 2 -s 5 & 
-./simulation_undirected -n 100 -K 2 -s 6 & 
-./simulation_undirected -n 100 -K 2 -s 7 & 
-./simulation_undirected -n 100 -K 2 -s 8 & 
-./simulation_undirected -n 100 -K 2 -s 9 & 
-./simulation_undirected -n 100 -K 2 -s 10 & 
-
-./simulation_undirected -n 200 -K 2 -s 1 & 
-./simulation_undirected -n 200 -K 2 -s 2 & 
-./simulation_undirected -n 200 -K 2 -s 3 & 
-./simulation_undirected -n 200 -K 2 -s 4 & 
-./simulation_undirected -n 200 -K 2 -s 5 & 
-./simulation_undirected -n 200 -K 2 -s 6 & 
-./simulation_undirected -n 200 -K 2 -s 7 & 
-./simulation_undirected -n 200 -K 2 -s 8 & 
-./simulation_undirected -n 200 -K 2 -s 9 & 
-./simulation_undirected -n 200 -K 2 -s 10 & 
-
-./simulation_undirected -n 500 -K 2 -s 1 & 
-./simulation_undirected -n 500 -K 2 -s 2 & 
-./simulation_undirected -n 500 -K 2 -s 3 & 
-./simulation_undirected -n 500 -K 2 -s 4 & 
-./simulation_undirected -n 500 -K 2 -s 5 & 
-./simulation_undirected -n 500 -K 2 -s 6 & 
-./simulation_undirected -n 500 -K 2 -s 7 & 
-./simulation_undirected -n 500 -K 2 -s 8 & 
-./simulation_undirected -n 500 -K 2 -s 9 & 
-./simulation_undirected -n 500 -K 2 -s 10 & 
-
-./simulation_undirected -n 1000 -K 2 -s 1 & 
-./simulation_undirected -n 1000 -K 2 -s 2 & 
-./simulation_undirected -n 1000 -K 2 -s 3 & 
-./simulation_undirected -n 1000 -K 2 -s 4 & 
-./simulation_undirected -n 1000 -K 2 -s 5 & 
-./simulation_undirected -n 1000 -K 2 -s 6 & 
-./simulation_undirected -n 1000 -K 2 -s 7 & 
-./simulation_undirected -n 1000 -K 2 -s 8 & 
-./simulation_undirected -n 1000 -K 2 -s 9 & 
-./simulation_undirected -n 1000 -K 2 -s 10 & 
-
-./simulation_undirected -n 2000 -K 2 -s 1 & 
-./simulation_undirected -n 2000 -K 2 -s 2 & 
-./simulation_undirected -n 2000 -K 2 -s 3 & 
-./simulation_undirected -n 2000 -K 2 -s 4 & 
-./simulation_undirected -n 2000 -K 2 -s 5 & 
-./simulation_undirected -n 2000 -K 2 -s 6 & 
-./simulation_undirected -n 2000 -K 2 -s 7 & 
-./simulation_undirected -n 2000 -K 2 -s 8 & 
-./simulation_undirected -n 2000 -K 2 -s 9 & 
-./simulation_undirected -n 2000 -K 2 -s 10 & 
-
-
+./sim_undirected.py -K 2 -s 111 & 
+./sim_undirected.py -K 2 -s 222 & 
+./sim_undirected.py -K 2 -s 333 & 
+./sim_undirected.py -K 2 -s 444 & 
+./sim_undirected.py -K 2 -s 555 & 
+./sim_undirected.py -K 2 -s 666 & 
+./sim_undirected.py -K 2 -s 777 & 
+./sim_undirected.py -K 2 -s 888 & 
+./sim_undirected.py -K 2 -s 999 & 
+./sim_undirected.py -K 2 -s 101010 &
