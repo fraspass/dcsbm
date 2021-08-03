d535aaefcb0ebe409c1b11dc7cf8336423b182de
fraspass <fraspass@outlook.com>
2021-08-03 21:31:30 +0200

    comments to zhu

--- zhu.py
+++ zhu.py
@@ -2,7 +2,9 @@
 import numpy as np
 from scipy.stats import norm
 
+## Calculate elbow of the scree-plot using the criterion of Zhu and Ghodsi (2006)
 def zhu(d):
+    d = np.sort(d)[::-1]
     p = len(d)
     profile_likelihood = np.zeros(p)
     for q in range(1,p-1):
@@ -12,7 +14,8 @@ def zhu(d):
         profile_likelihood[q] = norm.logpdf(d[:q],loc=mu1,scale=sd).sum() + norm.logpdf(d[q:],loc=mu2,scale=sd).sum()
     return profile_likelihood[1:p-1], np.argmax(profile_likelihood[1:p-1])+1
 
-def iterate_zhu(d,x=3):
+## Find the first x elbows of the scree-plot, iterating the criterion of Zhu and Ghodsi (2006)
+def iterate_zhu(d,x=4):
     results = np.zeros(x,dtype=int)
     results[0] = zhu(d)[1]
     for i in range(x-1):
