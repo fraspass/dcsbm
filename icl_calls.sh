68a7ea0cabc5b74c5f6ca5b0de04fdb3f1011ce3
fraspass <fraspass@outlook.com>
2021-08-04 15:48:05 +0200

    icl calls

---
+++ icl_calls.sh
@@ -0,0 +1,29 @@
+### m = 30
+
+# ICL1
+./icl.py --model 0 -m 30 -d 30 -K 20 -g 'icl1' &
+./icl.py --model 1 -m 30 -d 30 -K 20 -g 'icl1' &
+./icl.py --model 2 -m 30 -d 30 -K 20 -g 'icl1' &
+# ICL2
+./icl.py --model 0 -m 30 -d 30 -K 20 -g 'icl2' &
+./icl.py --model 1 -m 30 -d 30 -K 20 -g 'icl2' &
+./icl.py --model 2 -m 30 -d 30 -K 20 -g 'icl2' &
+# ICL3
+./icl.py --model 0 -m 30 -d 30 -K 20 -g 'icl3' &
+./icl.py --model 1 -m 30 -d 30 -K 20 -g 'icl3' &
+./icl.py --model 2 -m 30 -d 30 -K 20 -g 'icl3' &
+
+### m = 50
+
+# ICL1
+./icl.py --model 0 -m 50 -d 30 -K 20 -g 'icl1' &
+./icl.py --model 1 -m 50 -d 30 -K 20 -g 'icl1' &
+./icl.py --model 2 -m 50 -d 30 -K 20 -g 'icl1' &
+# ICL2
+./icl.py --model 0 -m 50 -d 30 -K 20 -g 'icl2' &
+./icl.py --model 1 -m 50 -d 30 -K 20 -g 'icl2' &
+./icl.py --model 2 -m 50 -d 30 -K 20 -g 'icl2' &
+# ICL3
+./icl.py --model 0 -m 50 -d 30 -K 20 -g 'icl3' &
+./icl.py --model 1 -m 50 -d 30 -K 20 -g 'icl3' &
+./icl.py --model 2 -m 50 -d 30 -K 20 -g 'icl3' &
